43f83ae35800548860e5f7498f6b6f62a166af86
jugiros <31550758+jugiros@users.noreply.github.com>
2023-06-25 18:37:33 -0500

    Actualización para dar estilos de visualización a la renderización de los memes.

--- src/components/ListMemes.vue
+++ src/components/ListMemes.vue
@@ -1,14 +1,20 @@
 <template>
-  <h1>{{ title }}</h1>
-  <p>{{ memes }}</p>
+  <h1 class="title">{{ title }}</h1>
+  <di class="row">
+    <templeate class="col-3" v-for="meme in memes" :key="meme.id">
+      <meme :meme="meme"></meme>
+    </templeate>
+  </di>
 </template>
 
 <script>
 import { useStore } from "vuex";
 import { onMounted, computed } from "vue";
+import Meme from "@/components/Meme.vue";
 
 export default {
   name: "ListMemes",
+  components: { Meme },
   setup() {
     const store = useStore();
     onMounted(() => {
@@ -25,4 +31,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.title {
+  text-align: center;
+}
+</style>
---
+++ src/components/Meme.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="meme">
+    <p>{{meme.name}}</p>
+    <img :src="meme.url">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Meme",
+  props: {
+    meme: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.meme {
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid gray;
+}
+.meme img {
+  max-height: 200px;
+  max-width: 200px;
+}
+</style>
