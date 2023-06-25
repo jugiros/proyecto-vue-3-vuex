81e19acd5e61040683bae456f3c954e6fbed66e8
jugiros <31550758+jugiros@users.noreply.github.com>
2023-06-25 18:44:16 -0500

    Actualización para integrar un selector de npumero de memes para filtrar el número de memes a visualizar.

--- src/components/ListMemes.vue
+++ src/components/ListMemes.vue
@@ -1,5 +1,13 @@
 <template>
   <h1 class="title">{{ title }}</h1>
+  <div class="total-show">
+    <select>
+      <option value="">All</option>
+      <option value="5">5</option>
+      <option value="10">10</option>
+      <option value="20">20</option>
+    </select>
+  </div>
   <di class="row">
     <templeate class="col-3" v-for="meme in memes" :key="meme.id">
       <meme :meme="meme"></meme>
@@ -17,12 +25,19 @@ export default {
   components: { Meme },
   setup() {
     const store = useStore();
-    onMounted(() => {
-      store.dispatch("getMemes");
-    });
+
     const memes = computed(() => {
       return store.state.memes;
     });
+
+    onMounted(() => {
+      store.dispatch("getMemes");
+    });
+
+    const changeTotalShow = (event) => {
+      const val = event.target.value;
+    };
+
     return {
       title: store.state.titleApp,
       memes,
@@ -34,5 +49,10 @@ export default {
 <style scoped>
 .title {
   text-align: center;
+}
+.total-show {
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 5px;
 }
 </style>
